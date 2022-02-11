7063371401f2f1d23e81ae9f55a8d15db2502999
Filip Lajszczak <filip@lajszczak.dev>
2022-02-11 16:25:08 +0000

    helpful messages when no api token is present both inside and outside PythonAnywhere.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,49 +1,75 @@
 import pytest
 import responses
 
-from pythonanywhere_core.base import call_api, get_api_endpoint
-from pythonanywhere_core.exceptions import AuthenticationError
+from pythonanywhere_core.base import (
+    call_api,
+    get_api_endpoint,
+    helpful_token_error_message,
+)
+from pythonanywhere_core.exceptions import AuthenticationError, NoTokenError
+
 
 @pytest.fixture
 def mock_requests(mocker):
     return mocker.patch("pythonanywhere_core.base.requests")
 
 
-class TestGetAPIEndpoint:
-
-    def test_defaults_to_pythonanywhere_dot_com_if_no_environment_variables(self):
-        assert get_api_endpoint() == "https://www.pythonanywhere.com/api/v0/user/{username}/{flavor}/"
-
-    def test_gets_domain_from_pythonanywhere_site_and_ignores_pythonanywhere_domain_if_both_set(self, monkeypatch):
-        monkeypatch.setenv("PYTHONANYWHERE_SITE", "www.foo.com")
-        monkeypatch.setenv("PYTHONANYWHERE_DOMAIN", "wibble.com")
-        assert get_api_endpoint() == "https://www.foo.com/api/v0/user/{username}/{flavor}/"
-
-    def test_gets_domain_from_pythonanywhere_domain_and_adds_on_www_if_set_but_no_pythonanywhere_site(
-        self, monkeypatch
-    ):
-        monkeypatch.setenv("PYTHONANYWHERE_DOMAIN", "foo.com")
-        assert get_api_endpoint() == "https://www.foo.com/api/v0/user/{username}/{flavor}/"
+def test_api_endpoint_defaults_to_pythonanywhere_dot_com_if_no_environment_variables():
+    assert get_api_endpoint() == "https://www.pythonanywhere.com/api/v0/user/{username}/{flavor}/"
 
 
-class TestCallAPI:
-    def test_raises_on_401(self, api_token, api_responses):
-        url = "https://foo.com/"
-        api_responses.add(responses.POST, url, status=401, body="nope")
-        with pytest.raises(AuthenticationError) as e:
-            call_api(url, "post")
-        assert str(e.value) == "Authentication error 401 calling API: nope"
+def test_gets_domain_from_pythonanywhere_site_and_ignores_pythonanywhere_domain_if_both_set(monkeypatch):
+    monkeypatch.setenv("PYTHONANYWHERE_SITE", "www.foo.com")
+    monkeypatch.setenv("PYTHONANYWHERE_DOMAIN", "wibble.com")
 
-    def test_passes_verify_from_environment(self, api_token, monkeypatch, mock_requests):
-        monkeypatch.setenv("PYTHONANYWHERE_INSECURE_API", "true")
+    assert get_api_endpoint() == "https://www.foo.com/api/v0/user/{username}/{flavor}/"
 
-        call_api("url", "post", foo="bar")
 
-        args, kwargs = mock_requests.request.call_args
-        assert kwargs["verify"] is False
+def test_gets_domain_from_pythonanywhere_domain_and_adds_on_www_if_set_but_no_pythonanywhere_site(monkeypatch):
+    monkeypatch.setenv("PYTHONANYWHERE_DOMAIN", "foo.com")
 
-    def test_verify_is_true_if_env_not_set(self, api_token, mock_requests):
-        call_api("url", "post", foo="bar")
+    assert get_api_endpoint() == "https://www.foo.com/api/v0/user/{username}/{flavor}/"
 
-        args, kwargs = mock_requests.request.call_args
-        assert kwargs["verify"] is True
+
+def test_raises_on_401(api_token, api_responses):
+    url = "https://foo.com/"
+    api_responses.add(responses.POST, url, status=401, body="nope")
+    with pytest.raises(AuthenticationError) as e:
+        call_api(url, "post")
+    assert str(e.value) == "Authentication error 401 calling API: nope"
+
+
+def test_passes_verify_from_environment(api_token, monkeypatch, mock_requests):
+    monkeypatch.setenv("PYTHONANYWHERE_INSECURE_API", "true")
+
+    call_api("url", "post", foo="bar")
+
+    _, kwargs = mock_requests.request.call_args
+    assert kwargs["verify"] is False
+
+
+def test_verify_is_true_if_env_not_set(api_token, mock_requests):
+    call_api("url", "post", foo="bar")
+
+    _, kwargs = mock_requests.request.call_args
+    assert kwargs["verify"] is True
+
+
+def test_raises_with_helpful_message_if_no_token_present(mocker):
+    mock_helpful_message = mocker.patch("pythonanywhere_core.base.helpful_token_error_message")
+    mock_helpful_message.return_value = "I'm so helpful"
+
+    with pytest.raises(NoTokenError) as exc:
+        call_api("blah", "get")
+
+    assert str(exc.value) == "I'm so helpful"
+
+
+def test_helpful_message_inside_pythonanywhere(monkeypatch):
+    monkeypatch.setenv("PYTHONANYWHERE_SITE", "www.foo.com")
+
+    assert "Oops, you don't seem to have an API token." in helpful_token_error_message()
+
+
+def test_helpful_message_outside_pythonanywhere():
+    assert "Oops, you don't seem to have an API_TOKEN environment variable set." in helpful_token_error_message()
