665d6c97e6dd244b0cc9d053be4171d2bf3d87d1
Filip Lajszczak <filip@lajszczak.dev>
2023-02-10 20:27:43 +0000

    extracts webapp api wrapper

--- pythonanywhere_core/webapp.py
+++ pythonanywhere_core/webapp.py
@@ -5,6 +5,7 @@ import getpass
 from pathlib import Path
 from textwrap import dedent
 
+from dateutil.parser import parse
 from snakesay import snakesay
 
 from pythonanywhere_core.base import call_api, get_api_endpoint, PYTHON_VERSIONS
@@ -13,7 +14,12 @@ from pythonanywhere_core.exceptions import SanityException, PythonAnywhereApiExc
 
 class Webapp:
     def __init__(self, domain: str) -> None:
+        self.endpoint = get_api_endpoint()
+        self.username = getpass.getuser()
+        self.files_url = self.endpoint.format(username=self.username, flavor="files")
+        self.webapps_url = self.endpoint.format(username=self.username, flavor="webapps")
         self.domain = domain
+        self.domain_url = f"{self.webapps_url}{self.domain}/"
 
     def __eq__(self, other: Webapp) -> bool:
         return self.domain == other.domain
@@ -35,9 +41,7 @@ class Webapp:
         if nuke:
             return
 
-        endpoint = get_api_endpoint().format(username=getpass.getuser(), flavor="webapps")
-        url = f"{endpoint}{self.domain}/"
-        response = call_api(url, "get")
+        response = call_api(self.domain_url, "get")
         if response.status_code == 200:
             raise SanityException(
                 f"You already have a webapp for {self.domain}.\n\nUse the --nuke option if you want to replace it."
@@ -45,23 +49,116 @@ class Webapp:
 
     def create(self, python_version: str, virtualenv_path: Path, project_path: Path, nuke: bool) -> None:
         print(snakesay("Creating web app via API"))
-        base_url = get_api_endpoint().format(username=getpass.getuser(), flavor="webapps")
-        domain_url = f"{base_url}{self.domain}/"
         if nuke:
-            call_api(domain_url, "delete")
-        patch_url = domain_url
+            call_api(self.domain_url, "delete")
         response = call_api(
-            base_url, "post", data={"domain_name": self.domain, "python_version": PYTHON_VERSIONS[python_version]}
+            self.webapps_url,
+            "post",
+            data={"domain_name": self.domain, "python_version": PYTHON_VERSIONS[python_version]},
         )
         if not response.ok or response.json().get("status") == "ERROR":
-            raise PythonAnywhereApiException(
-                f"POST to create webapp via API failed, got {response}:{response.text}"
-            )
+            raise PythonAnywhereApiException(f"POST to create webapp via API failed, got {response}:{response.text}")
         response = call_api(
-            patch_url, "patch", data={"virtualenv_path": virtualenv_path, "source_directory": project_path}
+            self.domain_url, "patch", data={"virtualenv_path": virtualenv_path, "source_directory": project_path}
         )
         if not response.ok:
             raise PythonAnywhereApiException(
-                "PATCH to set virtualenv path and source directory via API failed,"
-                "got {response}:{response_text}".format(response=response, response_text=response.text)
+                "PATCH to set virtualenv path and source directory via API failed," f"got {response}:{response.text}"
             )
+
+    def add_default_static_files_mappings(self, project_path: Path) -> None:
+        print(snakesay("Adding static files mappings for /static/ and /media/"))
+        url = f"{self.domain_url}static_files/"
+        call_api(url, "post", json=dict(url="/static/", path=str(Path(project_path) / "static")))
+        call_api(url, "post", json=dict(url="/media/", path=str(Path(project_path) / "media")))
+
+    def reload(self) -> None:
+        print(snakesay(f"Reloading {self.domain} via API"))
+        url = f"{self.domain_url}reload/"
+        response = call_api(url, "post")
+        if not response.ok:
+            if response.status_code == 409 and response.json()["error"] == "cname_error":
+                print(
+                    snakesay(
+                        dedent(
+                            """
+                            Could not find a CNAME for your website.  If you're using an A record,
+                            CloudFlare, or some other way of pointing your domain at PythonAnywhere
+                            then that should not be a problem.  If you're not, you should double-check
+                            your DNS setup.
+                        """
+                        )
+                    )
+                )
+                return
+            raise PythonAnywhereApiException(f"POST to reload webapp via API failed, got {response}:{response.text}")
+
+    def set_ssl(self, certificate: str, private_key: str) -> None:
+        print(snakesay(f"Setting up SSL for {self.domain} via API"))
+        url = f"{self.domain_url}ssl/"
+        response = call_api(url, "post", json={"cert": certificate, "private_key": private_key})
+        if not response.ok:
+            raise PythonAnywhereApiException(
+                dedent(
+                    f"""
+                    POST to set SSL details via API failed, got {response}:{response.text}
+                    If you just created an API token, you need to set the API_TOKEN environment variable or start a
+                    new console.  Also you need to have setup a `{self.domain}` PythonAnywhere webapp for this to work.
+                    """
+                )
+            )
+
+    def get_ssl_info(self) -> dict[str, Any]:
+        url = f"{self.domain_url}ssl/"
+        response = call_api(url, "get")
+        if not response.ok:
+            raise PythonAnywhereApiException(f"GET SSL details via API failed, got {response}:{response.text}")
+
+        result = response.json()
+        result["not_after"] = parse(result["not_after"])
+        return result
+
+    def delete_log(self, log_type: str, index: int = 0) -> None:
+        if index:
+            message = f"Deleting old (archive number {index}) {log_type} log file for {self.domain} via API"
+        else:
+            message = f"Deleting current {log_type} log file for {self.domain} via API"
+        print(snakesay(message))
+
+        if index == 1:
+            suffix = ".1"
+        elif index > 1:
+            suffix = f".{index}.gz"
+        else:
+            suffix = ""
+
+        base_log_url = f"{self.files_url}path/var/log/{self.domain}.{log_type}.log"
+        response = call_api(f"{base_log_url}{suffix}/", "delete")
+
+        if not response.ok:
+            raise PythonAnywhereApiException(f"DELETE log file via API failed, got {response}:{response.text}")
+
+    def get_log_info(self):
+        url = f"{self.files_url}tree/?path=/var/log/"
+        response = call_api(url, "get")
+        if not response.ok:
+            raise PythonAnywhereApiException(f"GET log files info via API failed, got {response}:{response.text}")
+        file_list = response.json()
+        log_types = ["access", "error", "server"]
+        logs = {"access": [], "error": [], "server": []}
+        log_prefix = f"/var/log/{self.domain}."
+        for file_name in file_list:
+            if type(file_name) == str and file_name.startswith(log_prefix):
+                log = file_name[len(log_prefix) :].split(".")
+                if log[0] in log_types:
+                    log_type = log[0]
+                    if log[-1] == "log":
+                        log_index = 0
+                    elif log[-1] == "1":
+                        log_index = 1
+                    elif log[-1] == "gz":
+                        log_index = int(log[-2])
+                    else:
+                        continue
+                    logs[log_type].append(log_index)
+        return logs
--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,8 +1,10 @@
 import getpass
 import json
+from datetime import datetime
 
 import pytest
 import responses
+from dateutil.tz import tzutc
 from urllib.parse import urlencode
 
 from pythonanywhere_core.base import get_api_endpoint, PYTHON_VERSIONS
@@ -10,9 +12,44 @@ from pythonanywhere_core.exceptions import SanityException, PythonAnywhereApiExc
 from pythonanywhere_core.webapp import Webapp
 
 
-def test_init():
-    app = Webapp("www.my-domain.com")
-    assert app.domain == "www.my-domain.com"
+@pytest.fixture
+def base_url():
+    return get_api_endpoint().format(username=getpass.getuser(), flavor="webapps")
+
+
+@pytest.fixture
+def domain():
+    return "www.domain.com"
+
+
+@pytest.fixture()
+def base_file_url():
+    return get_api_endpoint().format(username=getpass.getuser(), flavor='files')
+
+
+@pytest.fixture
+def base_log_url(base_file_url, domain):
+    return f"{base_file_url}path/var/log/{domain}"
+
+@pytest.fixture
+def domain_url(base_url, domain):
+    return f"{base_url}{domain}/"
+
+
+@pytest.fixture
+def ssl_url(domain_url):
+    return f"{domain_url}ssl/"
+
+
+@pytest.fixture
+def webapp(domain):
+    return Webapp(domain)
+
+
+def test_init(base_url, domain, domain_url, webapp):
+    assert webapp.domain == domain
+    assert webapp.webapps_url == base_url
+    assert webapp.domain_url == domain_url
 
 
 def test_compare_equal():
@@ -21,26 +58,6 @@ def test_compare_equal():
 
 def test_compare_not_equal():
     assert Webapp("www.my-domain.com") != Webapp("www.other-domain.com")
-
-
-@pytest.fixture
-def domain():
-    return "www.domain.com"
-
-
-@pytest.fixture
-def base_url():
-    return get_api_endpoint().format(username=getpass.getuser(), flavor='webapps')
-
-
-@pytest.fixture
-def domain_url(base_url, domain):
-    return f"{base_url}{domain}/"
-
-
-@pytest.fixture
-def webapp(domain):
-    return Webapp(domain)
 
 
 def test_does_not_complain_if_api_token_exists(api_token, api_responses, domain_url, webapp):
@@ -92,15 +109,11 @@ def test_does_post_to_create_webapp(api_responses, api_token, base_url, domain, 
     )
     api_responses.add(responses.PATCH, domain_url, status=200)
 
-    webapp.create(
-        "3.10", "/virtualenv/path", "/project/path", nuke=False
-    )
+    webapp.create("3.10", "/virtualenv/path", "/project/path", nuke=False)
 
     post = api_responses.calls[0]
     assert post.request.url == base_url
-    assert post.request.body == urlencode(
-        {"domain_name": domain, "python_version": PYTHON_VERSIONS["3.10"]}
-    )
+    assert post.request.body == urlencode({"domain_name": domain, "python_version": PYTHON_VERSIONS["3.10"]})
     assert post.request.headers["Authorization"] == f"Token {api_token}"
 
 
@@ -115,27 +128,19 @@ def test_does_patch_to_update_virtualenv_path_and_source_directory(
     )
     api_responses.add(responses.PATCH, domain_url, status=200)
 
-    webapp.create(
-        "3.10", "/virtualenv/path", "/project/path", nuke=False
-    )
+    webapp.create("3.10", "/virtualenv/path", "/project/path", nuke=False)
 
     patch = api_responses.calls[1]
     assert patch.request.url == domain_url
-    assert patch.request.body == urlencode(
-        {"virtualenv_path": "/virtualenv/path", "source_directory": "/project/path"}
-    )
+    assert patch.request.body == urlencode({"virtualenv_path": "/virtualenv/path", "source_directory": "/project/path"})
     assert patch.request.headers["Authorization"] == f"Token {api_token}"
 
 
-def test_raises_if_post_does_not_20x(api_responses, api_token, base_url, webapp):
-    api_responses.add(
-        responses.POST, base_url, status=500, body="an error"
-    )
+def test_raises_if_post_to_create_does_not_20x(api_responses, api_token, base_url, webapp):
+    api_responses.add(responses.POST, base_url, status=500, body="an error")
 
     with pytest.raises(PythonAnywhereApiException) as e:
-        webapp.create(
-            "3.10", "/virtualenv/path", "/project/path", nuke=False
-        )
+        webapp.create("3.10", "/virtualenv/path", "/project/path", nuke=False)
 
     assert "POST to create webapp via API failed" in str(e.value)
     assert "an error" in str(e.value)
@@ -156,9 +161,7 @@ def test_raises_if_post_returns_a_200_with_status_error(api_responses, api_token
     )
 
     with pytest.raises(PythonAnywhereApiException) as e:
-        webapp.create(
-            "3.10", "/virtualenv/path", "/project/path", nuke=False
-        )
+        webapp.create("3.10", "/virtualenv/path", "/project/path", nuke=False)
 
     assert "POST to create webapp via API failed" in str(e.value)
     assert "bad things happened" in str(e.value)
@@ -179,22 +182,15 @@ def test_raises_if_patch_does_not_20x(api_responses, api_token, base_url, domain
     )
 
     with pytest.raises(PythonAnywhereApiException) as e:
-        webapp.create(
-            "3.7", "/virtualenv/path", "/project/path", nuke=False
-        )
+        webapp.create("3.7", "/virtualenv/path", "/project/path", nuke=False)
 
-    assert (
-        "PATCH to set virtualenv path and source directory via API failed"
-        in str(e.value)
-    )
+    assert "PATCH to set virtualenv path and source directory via API failed" in str(e.value)
     assert "an error" in str(e.value)
 
 
 def test_does_delete_first_for_nuke_call(api_responses, api_token, base_url, domain_url, webapp):
     api_responses.add(responses.DELETE, domain_url, status=200)
-    api_responses.add(
-        responses.POST, base_url, status=201, body=json.dumps({"status": "OK"})
-    )
+    api_responses.add(responses.POST, base_url, status=201, body=json.dumps({"status": "OK"}))
     api_responses.add(responses.PATCH, domain_url, status=200)
 
     webapp.create("3.10", "/virtualenv/path", "/project/path", nuke=True)
@@ -207,9 +203,240 @@ def test_does_delete_first_for_nuke_call(api_responses, api_token, base_url, dom
 
 def test_ignores_404_from_delete_call_when_nuking(api_responses, api_token, base_url, domain_url, webapp):
     api_responses.add(responses.DELETE, domain_url, status=404)
-    api_responses.add(
-        responses.POST, base_url, status=201, body=json.dumps({"status": "OK"})
-    )
+    api_responses.add(responses.POST, base_url, status=201, body=json.dumps({"status": "OK"}))
     api_responses.add(responses.PATCH, domain_url, status=200)
 
     webapp.create("3.10", "/virtualenv/path", "/project/path", nuke=True)
+
+
+def test_does_two_posts_to_static_files_endpoint(api_token, api_responses, domain_url, webapp):
+    static_files_url = f"{domain_url}static_files/"
+    api_responses.add(responses.POST, static_files_url, status=201)
+    api_responses.add(responses.POST, static_files_url, status=201)
+
+    webapp.add_default_static_files_mappings("/project/path")
+
+    post1 = api_responses.calls[0]
+    assert post1.request.url == static_files_url
+    assert post1.request.headers["content-type"] == "application/json"
+    assert post1.request.headers["Authorization"] == f"Token {api_token}"
+    assert json.loads(post1.request.body.decode("utf8")) == {
+        "url": "/static/",
+        "path": "/project/path/static",
+    }
+    post2 = api_responses.calls[1]
+    assert post2.request.url == static_files_url
+    assert post2.request.headers["content-type"] == "application/json"
+    assert post2.request.headers["Authorization"] == f"Token {api_token}"
+    assert json.loads(post2.request.body.decode("utf8")) == {
+        "url": "/media/",
+        "path": "/project/path/media",
+    }
+
+
+def test_does_post_to_reload_url(api_responses, api_token, domain_url, webapp):
+    reload_url = f"{domain_url}reload/"
+    api_responses.add(responses.POST, reload_url, status=200)
+
+    webapp.reload()
+
+    post = api_responses.calls[0]
+    assert post.request.url == reload_url
+    assert post.request.body is None
+    assert post.request.headers["Authorization"] == f"Token {api_token}"
+
+
+def test_raises_if_post_does_not_20x_that_is_not_a_cname_error(api_responses, api_token, domain_url, webapp):
+    reload_url = f"{domain_url}reload/"
+    api_responses.add(responses.POST, reload_url, status=404, body="nope")
+
+    with pytest.raises(PythonAnywhereApiException) as e:
+        webapp.reload()
+
+    assert "POST to reload webapp via API failed" in str(e.value)
+    assert "nope" in str(e.value)
+
+
+def test_does_not_raise_if_post_responds_with_a_cname_error(api_responses, api_token, domain_url, webapp):
+    reload_url = f"{domain_url}reload/"
+    api_responses.add(
+        responses.POST,
+        reload_url,
+        status=409,
+        json={"status": "error", "error": "cname_error"},
+    )
+
+    webapp.reload()  # Should not raise
+
+
+def test_does_post_to_ssl_url(api_responses, api_token, domain_url, webapp):
+    ssl_url = f"{domain_url}ssl/"
+    api_responses.add(responses.POST, ssl_url, status=200)
+    certificate = "certificate data"
+    private_key = "private key data"
+
+    webapp.set_ssl(certificate, private_key)
+
+    post = api_responses.calls[0]
+    assert post.request.url == ssl_url
+    assert json.loads(post.request.body.decode("utf8")) == {
+        "private_key": "private key data",
+        "cert": "certificate data",
+    }
+    assert post.request.headers["Authorization"] == f"Token {api_token}"
+
+
+def test_raises_if_post_to_ssl_does_not_20x(api_responses, api_token, ssl_url, webapp):
+    api_responses.add(responses.POST, ssl_url, status=404, body="nope")
+
+    with pytest.raises(PythonAnywhereApiException) as e:
+        webapp.set_ssl("foo", "bar")
+
+    assert "POST to set SSL details via API failed" in str(e.value)
+    assert "nope" in str(e.value)
+
+
+def test_returns_json_from_server_having_parsed_expiry_with_z_for_utc_and_no_separators(
+    api_responses, api_token, ssl_url, webapp
+):
+    api_responses.add(
+        responses.GET,
+        ssl_url,
+        status=200,
+        body=json.dumps(
+            {
+                "not_after": "20180824T171623Z",
+                "issuer_name": "PythonAnywhere test CA",
+                "subject_name": "www.mycoolsite.com",
+                "subject_alternate_names": ["www.mycoolsite.com", "mycoolsite.com"],
+            }
+        ),
+    )
+
+    assert webapp.get_ssl_info() == {
+        "not_after": datetime(2018, 8, 24, 17, 16, 23, tzinfo=tzutc()),
+        "issuer_name": "PythonAnywhere test CA",
+        "subject_name": "www.mycoolsite.com",
+        "subject_alternate_names": ["www.mycoolsite.com", "mycoolsite.com"],
+    }
+
+    get = api_responses.calls[0]
+    assert get.request.method == "GET"
+    assert get.request.url == ssl_url
+    assert get.request.headers["Authorization"] == f"Token {api_token}"
+
+
+def test_returns_json_from_server_having_parsed_expiry_with_timezone_offset_and_separators(
+    api_responses, api_token, ssl_url, webapp
+):
+    api_responses.add(
+        responses.GET,
+        ssl_url,
+        status=200,
+        body=json.dumps(
+            {
+                "not_after": "2018-08-24T17:16:23+00:00",
+                "issuer_name": "PythonAnywhere test CA",
+                "subject_name": "www.mycoolsite.com",
+                "subject_alternate_names": ["www.mycoolsite.com", "mycoolsite.com"],
+            }
+        ),
+    )
+
+    assert webapp.get_ssl_info() == {
+        "not_after": datetime(2018, 8, 24, 17, 16, 23, tzinfo=tzutc()),
+        "issuer_name": "PythonAnywhere test CA",
+        "subject_name": "www.mycoolsite.com",
+        "subject_alternate_names": ["www.mycoolsite.com", "mycoolsite.com"],
+    }
+
+    get = api_responses.calls[0]
+    assert get.request.method == "GET"
+    assert get.request.url == ssl_url
+    assert get.request.headers["Authorization"] == f"Token {api_token}"
+
+
+def test_raises_if_get_does_not_return_200(api_responses, api_token, ssl_url, webapp):
+    api_responses.add(responses.GET, ssl_url, status=404, body="nope")
+
+    with pytest.raises(PythonAnywhereApiException) as e:
+        webapp.get_ssl_info()
+
+    assert "GET SSL details via API failed, got" in str(e.value)
+    assert "nope" in str(e.value)
+
+
+def test_delete_current_access_log(api_responses, api_token, base_log_url, webapp):
+    expected_url = f"{base_log_url}.access.log/"
+    api_responses.add(responses.DELETE, expected_url, status=200)
+
+    webapp.delete_log(log_type="access")
+
+    post = api_responses.calls[0]
+    assert post.request.url == expected_url
+    assert post.request.body is None
+    assert post.request.headers["Authorization"] == f"Token {api_token}"
+
+
+def test_delete_old_access_log(api_responses, api_token, base_log_url, webapp):
+    expected_url = f"{base_log_url}.access.log.1/"
+    api_responses.add(responses.DELETE, expected_url, status=200)
+
+    webapp.delete_log(log_type="access", index=1)
+
+    post = api_responses.calls[0]
+    assert post.request.url == expected_url
+    assert post.request.body is None
+    assert post.request.headers["Authorization"] == f"Token {api_token}"
+
+
+def test_raises_if_log_delete_does_not_20x(api_responses, api_token, base_log_url, webapp):
+    expected_url = f"{base_log_url}.access.log/"
+    api_responses.add(responses.DELETE, expected_url, status=404, body="nope")
+
+    with pytest.raises(PythonAnywhereApiException) as e:
+        webapp.delete_log(log_type="access")
+
+    assert "DELETE log file via API failed" in str(e.value)
+    assert "nope" in str(e.value)
+
+
+def test_get_list_of_logs(api_responses, api_token, base_file_url, domain, webapp):
+    expected_url = f"{base_file_url}tree/?path=/var/log/"
+    api_responses.add(
+        responses.GET,
+        expected_url,
+        status=200,
+        body=json.dumps(
+            [
+                "/var/log/blah",
+                f"/var/log/{domain}.access.log",
+                f"/var/log/{domain}.access.log.1",
+                f"/var/log/{domain}.access.log.2.gz",
+                f"/var/log/{domain}.error.log",
+                f"/var/log/{domain}.error.log.1",
+                f"/var/log/{domain}.error.log.2.gz",
+                f"/var/log/{domain}.server.log",
+                f"/var/log/{domain}.server.log.1",
+                f"/var/log/{domain}.server.log.2.gz",
+            ]
+        ),
+    )
+
+    logs = webapp.get_log_info()
+
+    post = api_responses.calls[0]
+    assert post.request.url == expected_url
+    assert post.request.headers["Authorization"] == f"Token {api_token}"
+    assert logs == {"access": [0, 1, 2], "error": [0, 1, 2], "server": [0, 1, 2]}
+
+
+def test_raises_if_get_does_not_20x(api_responses, api_token, base_file_url, webapp):
+    expected_url = f"{base_file_url}tree/?path=/var/log/"
+    api_responses.add(responses.GET, expected_url, status=404, body="nope")
+
+    with pytest.raises(PythonAnywhereApiException) as e:
+        webapp.get_log_info()
+
+    assert "GET log files info via API failed" in str(e.value)
+    assert "nope" in str(e.value)
